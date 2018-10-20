82d112a5be4202567d698b60fc2633ce61b09824
Andrew McDonnell <bugs@andrewmcdonnell.net>
2018-10-20 20:17:55 +1030

    More prototyping

--- src/load-story.js
+++ src/load-story.js
@@ -14,7 +14,20 @@ exports.loadStory = (sourceJson) => {
 
   console.log(initScript);
 
+  const title = document.createElement('title');
+  title.innerHTML = 'Title hello world';
+
+  const intro = document.createElement('page');
+  intro.setAttribute('id', 'intro');
+  intro.innerHTML = `
+    <p>Hurrying through the rainswept November night, you're glad to see the bright lights of the Opera House. It's surprising that there aren't more people about but, hey, what do you expect in a cheap demo game...?</p>
+    <div class="center">Cloak of Darkness</div>
+    <div class="center">A basic IF demonstration.</div>`;
+
+
   story.appendChild(initScript);
+  story.appendChild(title);
+  story.appendChild(intro);
   story.setAttribute('id', 'story');
 
   return story;
