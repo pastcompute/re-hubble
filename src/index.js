95102ac38af6ba3e65e00d88bd24c69c85468cb3
Andrew McDonnell <bugs@andrewmcdonnell.net>
2018-10-20 16:53:18 +1030

    Devise a mechanism to generate the story from a json description instead of hard coding it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 console.log('Hello, World');
 
 $(function () {
+
+  const story = document.getElementById('story');
+
+  var el;
+  
+  el = document.createElement('initscript');
+  el.innerHTML = `
+  story.messageDamage  = 0;
+  story.messageScrewed = function () { return story.messageDamage < 2; };
+  story.cloakPutOn     = false;
+  story.cloakJustPutOn = false;
+`;
+
+  story.appendChild(el);
+
+  console.log(story);
+
+
+
   new DedalusWeb({
       domSource         : $('#story'),
       domTarget         : $('#host'),
