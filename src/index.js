3e9c31a599a07dd4bc5be24635b89bdd5d0b3c65
Andrew McDonnell <andrew@platfarm.com>
2018-10-20 22:22:24 +1030

    Engine work in progress

--- src/index.js
+++ src/index.js
@@ -4,26 +4,23 @@ console.log('Hello, World');
 
 $(function () {
 
-  const story = StoryLoader.loadDemoStory('story.json');
-  const container = document.getElementById('story-container');
-
-  container.appendChild(story);
-
-  console.log(container);
-
-
-
-  new DedalusWeb({
-      domSource         : $('#story'),
-      domTarget         : $('#host'),
-      titleTarget       : $('#title'),
-      inventoryTarget   : $('#inventoryHost'),
-      interactionTarget : $('#interactionHost'),
-      undoTarget        : $('#undoHost'),
-      undoStageTarget   : $('#undoStageHost'),
-      saveTarget        : $('#saveHost'),
-      restoreTarget     : $('#restoreHost'),
-      resetTarget       : $('#reseteHost'),
+  // const story = StoryLoader.loadDemoStory('story.json');
+  StoryLoader.loadStoryJson('Welcome to Re:Hubble', 'assets/map.json').then(story => {
+    const container = document.getElementById('story-container');
+    container.appendChild(story);
+    // console.log(container);
+    new DedalusWeb({
+        domSource         : $('#story'),
+        domTarget         : $('#host'),
+        titleTarget       : $('#title'),
+        inventoryTarget   : $('#inventoryHost'),
+        interactionTarget : $('#interactionHost'),
+        undoTarget        : $('#undoHost'),
+        undoStageTarget   : $('#undoStageHost'),
+        saveTarget        : $('#saveHost'),
+        restoreTarget     : $('#restoreHost'),
+        resetTarget       : $('#reseteHost'),
+    });
   });
 });
 
--- src/load-story.js
+++ src/load-story.js
@@ -1,10 +1,12 @@
 const exports = module.exports = {};
+const d3 = require('d3-fetch');
+const _ = require('lodash/core');
 
-function genDemoPage(id, inner, ...classes) {
+function genPage(id, inner, ...classes) {
   const el = document.createElement('page');
   el.setAttribute('id', id);
   el.innerHTML = inner;
-  if (classes) {
+  if (classes && classes.length > 0) {
     el.classList.add(...classes);
   }
   return el;
@@ -27,13 +29,13 @@ exports.loadDemoStory = (sourceJson) => {
   const title = document.createElement('title');
   title.innerHTML = 'Title hello world';
 
-  const intro = genDemoPage('intro',`
+  const intro = genPage('intro',`
     <p>Hurrying through the rainswept November night, you're glad to see the bright lights of the Opera House. It's surprising that there aren't more people about but, hey, what do you expect in a cheap demo game...?</p>
     <div class="center">Cloak of Darkness</div>
     <div class="center">A basic IF demonstration.</div>`
   );
 
-  const foyer = genDemoPage('foyer',`
+  const foyer = genPage('foyer',`
     <p>You are standing in a spacious hall, splendidly decorated in red and gold, with glittering chandeliers overhead.
     There is an entrance from the <show paragraph="street">street</show>,
     and there are doorways <turn to="bar" id="south">south</turn> and <turn to="cloakroom">west</turn>.</p>
@@ -54,3 +56,69 @@ exports.loadDemoStory = (sourceJson) => {
   return story;
 }
 
+function parseMap(json, titleInner) {
+  // console.log(json);
+  const story = document.createElement('div');
+  story.setAttribute('id', 'story');
+
+  const initScript = document.createElement('initscript');
+  initScript.innerHTML = `
+    story.messageDamage  = 0;
+    story.messageScrewed = function () { return story.messageDamage < 2; };
+    story.cloakPutOn     = false;
+    story.cloakJustPutOn = false;`;
+
+  console.log(initScript);
+
+  const title = document.createElement('title');
+  title.innerHTML = titleInner;
+
+  const nodes = json.nodes;
+  // console.log(nodes);
+
+  // For now, intro is type start
+  // const start1 = _.find(nodes, (o) => o.type === 'intro');
+  // if (!start1) { console.error('type=intro not found in json'); }
+  // const intro = genPage('intro', `<h1>${start1.title}</h1><p>${start1.body}</p>`);
+  // const foyer1 = _.find(nodes, (o) => o.type === 'first');
+  // if (!foyer1) { console.error('type=first not found in json'); }
+  // let hh = `<h1>${foyer1.title}</h1><p>${foyer1.body}</p>`;
+  // if (foyer1.image) {
+  //   hh += `<img class="nebula" src="assets/images/${foyer1.image}">`
+  // }
+  // const foyer = genPage('foyer', hh, 'first');
+
+  let intro;
+  let first;
+  const pages = [];
+  _.each(nodes, (v, k) => {
+    console.log(`Node id=${v.id} type=${v.type} title=${v.title}`);
+    const txt = `<h2>${v.title}</h2><p>${v.body}</p>`
+    if (v.type === 'intro') {
+      intro = genPage('intro', txt);
+      return;
+    }
+    if (v.image) {
+      txt += `<img class="nebula" src="assets/images/${foyer1.image}">`;
+    }
+    const classes = [];
+    if (!first && v.type === 'foyer') {
+      classes.push('first');
+      first = true;
+    }
+    pages,push(genPage(v.id, txt, classes));
+  });
+
+
+  story.appendChild(initScript);
+  story.appendChild(title);
+  story.appendChild(intro);
+  pages.each((v) => story.appendChild(v));
+  return story;
+}
+
+// TODO: load title from json
+exports.loadStoryJson = (titleInner, sourceJson) => {
+  console.log(`Load Story from ${sourceJson}`);
+  return new Promise((resolve, reject) => d3.json(sourceJson).then((json) => { resolve(parseMap(json, titleInner)); }));
+}
