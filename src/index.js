fb405791511c13a6f72b98fef5fa814e5c5932a4
Andrew McDonnell <bugs@andrewmcdonnell.net>
2018-10-20 16:21:13 +1030

    More structural preparation, try and launch Dedalus

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 console.log('Hello, World');
 
+$(function () {
+  new DedalusWeb({
+      domSource         : $('#story'),
+      domTarget         : $('#host'),
+      titleTarget       : $('#title'),
+      inventoryTarget   : $('#inventoryHost'),
+      interactionTarget : $('#interactionHost'),
+      undoTarget        : $('#undoHost'),
+      undoStageTarget   : $('#undoStageHost'),
+      saveTarget        : $('#saveHost'),
+      restoreTarget     : $('#restoreHost'),
+      resetTarget       : $('#reseteHost'),
+  });
+});
+
 module.exports = {}
